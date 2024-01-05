90475a9f64b71619a87ee5a00c337125f41303a5
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2024-01-05 16:02:08 +0000

    adds class 5 files

---
+++ class-5.js
@@ -0,0 +1,44 @@
+// Data Types and Variable Declaration
+
+/**
+ * String
+ * Anything wrapped around quotation marks
+ */
+
+/** created a variable with value `it has been 30 years` */
+const text = "it has been 30 years"
+console.log(text)
+
+/**
+ * Integers/Numbers
+ * Integers 0-9
+ */
+
+/** created a variable with value `90` */
+const age = 90
+console.log(age)
+
+/**
+ * Arrays/List
+ * It is a collection of items (integers, numbers, or mixture of both, or any other data type)
+ */
+
+const stringOnlyArray = ['a', 'b', 'c']
+console.log(stringOnlyArray)
+
+const numberOnlyArray = [1, 2, 3]
+console.log(numberOnlyArray)
+
+const mixedArray = ['a', 2, [1, 2], ['a', 'b']]
+console.log(mixedArray)
+
+/**
+ * Object/Dictionary/Mapping
+ * Consists of a key-value structure
+ */
+
+const dictionary = {
+    python: "python is a programming language",
+    php: "php is a weird language"
+}
+console.log(dictionary)
